4751b564d16c50003817af40d9360bd0546889bc
Lancy Hamiliya <lancyhami09@gmail.com>
2026-07-21 09:45:46 +0530

    Implement stack using queues in MyStack class

---
+++ Day 2 (HW) - ImplementStackUsingQueues.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class MyStack {
+
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.offer(x);
+
+        // Rotate the queue so the new element comes to the front
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.offer(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
